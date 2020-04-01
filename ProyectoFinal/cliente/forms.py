b79a9b2091152f9b51e7d6102a839bb4acc8f101
Alexis Navarrete <alexisnpfc@ciencias.unam.mx>
2020-04-01 12:52:50 -0600

    form registro cliente corrección

--- ProyectoFinal/cliente/forms.py
+++ ProyectoFinal/cliente/forms.py
@@ -7,13 +7,7 @@ from django.contrib.auth import authenticate
 
 class RegistroCliente(forms.Form):
     """Sign up new user form."""
-    id_cliente = models.AutoField(primary_key=True, db_column='id_cliente')
-    nombre_cliente = models.CharField(max_length=64)
-    apellido_pa_cliente = models.CharField(max_length=100)
-    apellido_ma_cliente = models.CharField(max_length=100)
-    correo_cliente = models.EmailField(max_length=254)
-    contra_cliente = models.CharField(max_length=16)
-    telefono_cliente = models.CharField(max_length=10)
+    
 
     nombre = forms.CharField(max_length=64)
     paterno = forms.CharField(max_length=100)
@@ -22,7 +16,7 @@ class RegistroCliente(forms.Form):
     contrasenia = forms.CharField(max_length=16)
     telefono = forms.CharField(max_length=10)
    
-    def clean_email(self):
+    def clean_correo(self):
         """Validate that the email doesn't exist in the database."""
         data = self.cleaned_data["correo"]
         if User.objects.filter(email=data).count() > 0:
