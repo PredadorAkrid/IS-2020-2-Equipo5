a49cafd9b1adb55cb77caeacbc5348304630c7c5
Dulce <42220874+DulceAmaya@users.noreply.github.com>
2020-04-03 23:31:46 -0600

    Primer modelo Repartidor

--- ProyectoFinal/repartidor/models.py
+++ ProyectoFinal/repartidor/models.py
@@ -1,3 +1,20 @@
+"""Modelos Repartidor"""
+#Django
 from django.db import models
 
-# Create your models here.
+class Repartidor(models.Model):
+	"""Modelo Repartidor"""
+
+	id_repatidor = models.AutoField(primary_key=True, db_column='id_repatidor')
+	nombre_repartidor = models.CharField(max_length=64)
+	apellido_paterno_repartidor = models.CharField(max_length=100)
+	apellido_materno_repartidor = models.CharField(max_length=100)
+	correo_repartidor = models.EmailField(max_length=254)
+	password_repartidor = models.CharField(max_length=16)
+
+	def __str__(self):
+		return self.nombre_repartidor + " " + self.apellido_paterno_repartidor
+
+	def __repr__(self):
+		"""Get str representation"""
+		return self.__str__()
