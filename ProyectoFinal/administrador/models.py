72ccc6fe7088e65d943b4e865578d877f3f76233
Dulce <42220874+DulceAmaya@users.noreply.github.com>
2020-04-07 14:39:38 -0500

    Orden

--- ProyectoFinal/administrador/models.py
+++ ProyectoFinal/administrador/models.py
@@ -1,13 +1,15 @@
 from django.db import models
-from cliente import models as cliente_models
-from repartidor  import models as repartidor_models
+#from cliente.models import *
+from cliente import models
+from repartidor  import models
+from platillo import  models
+from repartidor.models import *
 # Create your models here.
 
-
-#class Orden(models.Model):
-#	id_orden = models.AutoField(primary_key=True)
-#	precio_orden = models.IntegerField(null=False, default = 0)
-#	id_cliente = models.ForeignKey('cliente_models.Cliente')
-#	id_repartidor = models.ForeignKey('repartidor_models.Repartidor')
-#	id_platillo  = models.ManyToManyField('Platillo', related_name='platillos')
-#	direccion_entrega  = models.CharField(null=false, max_length=200)
+class Orden(models.Model):
+	id_orden = models.AutoField(primary_key=True)
+	precio_orden = models.IntegerField(null=False, default = 0)
+	id_cliente = models.ForeignKey('cliente.Cliente', on_delete=models.SET_DEFAULT, default=0)
+	id_repartidor = models.ForeignKey('repartidor.Repartidor',on_delete= models.SET_DEFAULT, null=True, default = 0)
+	id_platillo  = models.ManyToManyField('platillo.Platillo', null=True)
+	direccion_entrega  = models.CharField(null=False, max_length=200)
