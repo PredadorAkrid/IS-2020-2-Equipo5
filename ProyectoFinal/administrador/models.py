9fa2d491973f67093e7cb5fed480ba38cfd53783
Alexis Navarrete <alexisnpfc@ciencias.unam.mx>
2020-04-07 15:40:15 -0500

    agregada tabla de estado

--- ProyectoFinal/administrador/models.py
+++ ProyectoFinal/administrador/models.py
@@ -12,4 +12,16 @@ class Orden(models.Model):
 	id_cliente = models.ForeignKey('cliente.Cliente', on_delete=models.SET_DEFAULT, default=0)
 	id_repartidor = models.ForeignKey('repartidor.Repartidor',on_delete= models.SET_DEFAULT, null=True, default = 0)
 	id_platillo  = models.ManyToManyField('platillo.Platillo', null=True)
+	id_estado =    models.ForeignKey('EstadoOrden',  on_delete=models.SET_DEFAULT, default=0)
 	direccion_entrega  = models.CharField(null=False, max_length=200)
+	#estado_orden = models.VarChar()
+
+class EstadoOrden(models.Model):
+	"""docstring for EstadoOrden"""
+	id_estado = models.IntegerField(primary_key=True)
+
+	descripcion_estado = models.CharField(max_length=30)
+
+	def __init__(self, arg):
+		super(EstadoOrden, self).__init__()
+		self.arg = arg
