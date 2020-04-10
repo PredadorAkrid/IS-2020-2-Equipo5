7759a5e288187d7d6af6d00e1a2e3273bc688c97
Dulce <42220874+DulceAmaya@users.noreply.github.com>
2020-04-09 21:38:39 -0500

    Corrección

--- ProyectoFinal/repartidor/models.py
+++ ProyectoFinal/repartidor/models.py
@@ -3,26 +3,6 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-'''
-
-class Repartidor(models.Model):
-	"""Modelo Repartidor"""
-
-	id_repatidor = models.AutoField(primary_key=True, db_column='id_repatidor')
-	nombre_repartidor = models.CharField(max_length=64)
-	apellido_paterno_repartidor = models.CharField(max_length=100)
-	apellido_materno_repartidor = models.CharField(max_length=100)
-	correo_repartidor = models.EmailField(max_length=254)
-	password_repartidor = models.CharField(max_length=16, null=True)
-
-	def __str__(self):
-		return self.nombre_repartidor + " " + self.apellido_paterno_repartidor
-
-	def __repr__(self):
-		"""Representación en cadena de un Repartidor"""
-		return self.__str__()
-
-'''
 class Repartidor(models.Model):
 
 	user = models.OneToOneField(User, on_delete=models.CASCADE, unique=True)
