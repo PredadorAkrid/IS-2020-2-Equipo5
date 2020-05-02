b890441b9e7c2c282e8c41bde048bd035891c47b
Alexis Navarrete <alexisnpfc@ciencias.unam.mx>
2020-05-01 21:35:59 -0500

    asdasdas

--- ProyectoFinal/ProyectoFinal/settings.py
+++ ProyectoFinal/ProyectoFinal/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -130,3 +131,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'deliverandeats@gmail.com'
 EMAIL_HOST_PASSWORD = 'deliver&eats1'
 EMAIL_PORT = 587
+django_heroku.settings(locals())
