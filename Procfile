7dcdd6c9b8c6623214d43f5dfd011359a8b4f2b8
Alexis Navarrete <alexisnpfc@ciencias.unam.mx>
2020-05-05 18:18:42 -0500

    cambios para entrega 01

--- Procfile
+++
@@ -1,2 +0,0 @@
-release: python manage.py makemigrations && manage.py migrate --noinput 
-web: gunicorn nombre_del_proyecto.wsgi:application --log-file -
--- ProyectoFinal/ProyectoFinal/settings.py
+++ ProyectoFinal/ProyectoFinal/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
+#import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -139,4 +139,5 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'deliverandeats@gmail.com'
 EMAIL_HOST_PASSWORD = 'deliver&eats1'
 EMAIL_PORT = 587
-django_heroku.settings(locals())
+
+#django_heroku.settings(locals())
