015df6c162394987974c542c0e00c1602b0a114f
Alexis Navarrete <alexisnpfc@ciencias.unam.mx>
2020-06-16 17:03:03 -0500

    cambios locales

--- ProyectoFinal/cliente/forms.py
+++ ProyectoFinal/cliente/forms.py
@@ -70,7 +70,8 @@ class ClienteRegistroForm(UserCreationForm):
             'id': 'id_telefono',
             'class': 'input100',
             'name': 'telefono',
-            'placeholder': 'Telefono'})
+            'placeholder': 'Telefono',
+            'pattern' : '[0-9]+',})
         self.fields['email'].widget = TextInput(attrs={
             'id': 'id_email',
             'class': 'input100',
